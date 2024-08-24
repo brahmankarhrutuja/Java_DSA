ae3d553523bf0caf236a7868796f3a669f939c5f
Hrutuja Rahul Brahmankar <141763509+brahmankarhrutuja@users.noreply.github.com>
2024-08-24 10:28:18 +0000

    Added two more files

---
+++ 01-Array/1-WithCodeRedundancy/Program1.java
@@ -0,0 +1,14 @@
+class Program1 {
+    public static void main(String[] args) {
+        //created new array
+        int[] arr = new int[2];
+
+        //stores data
+        arr[0] = 1;
+        arr[1] = 2;
+
+        //prints the data
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
+}
